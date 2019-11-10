9c02d68acc21f919cc846fc472101e4ecb7ffd18
Aleksandr Kolbasov <kolbasov.a.a@edu.mirea.ru>
2019-11-10 20:40:30 +0300

    update PrimeGen

--- src/cryptography/utils/primesGenerator/PrimesGenerator.java
+++ src/cryptography/utils/primesGenerator/PrimesGenerator.java
@@ -12,11 +12,14 @@ public class PrimesGenerator {
     public static long getRandomPrime(){
         SecureRandom secureRandom = new SecureRandom();
         long randomLong = secureRandom.nextLong();
+        // randomLong % 2 != 0
+        randomLong |= 0b1;
         do {
             // TODO : SafePrime (p = 2 * q + 1)
             if (MillerRabinTest.isProbablePrime(randomLong, 5))
                 return randomLong;
-            randomLong +=1;
+            // randomLong % 2 != 0 ==> +2
+            randomLong += 2;
         } while (true);
     }
 
