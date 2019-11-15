95012b003c3e1d2f98905682276d96fc63eac572
Aleksandr Kolbasov <kolbasov.a.a@edu.mirea.ru>
2019-11-15 20:37:37 +0300

    Начало SRP6

---
+++ src/cryptography/srp6/Client.java
@@ -0,0 +1,22 @@
+package cryptography.srp6;
+
+import javax.crypto.SecretKey;
+import javax.crypto.Cipher;
+import java.security.KeyStore;
+import java.security.SecureRandom;
+/*
+ * Copyright © 2019 Alexander Kolbasov
+ */
+
+public class Client {
+    // <http://srp.stanford.edu/design.html>
+    // <https://habr.com/ru/post/121021/>
+
+    void signUp(String password){
+        SecureRandom secureRandom = new SecureRandom();
+
+    }
+
+    void signIn(){
+    }
+}
---
+++ src/cryptography/srp6/Hash.java
@@ -0,0 +1,12 @@
+package cryptography.srp6;
+
+/*
+ * Copyright © 2019 Alexander Kolbasov
+ */
+
+public class Hash {
+    byte[] hash(byte[]... bytes){
+        // TODO
+        throw new RuntimeException();
+    }
+}
