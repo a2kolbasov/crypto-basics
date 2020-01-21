f200e6cd538f0bdeda58e042e9bb354a6fc88c72
Aleksandr Kolbasov <kolbasov.a.a@edu.mirea.ru>
2020-01-21 16:09:07 +0300

    RSA - генерация ключей

---
+++ src/cryptography/rsa/RSA.java
@@ -0,0 +1,28 @@
+/*
+ * Copyright © 2019 Alexander Kolbasov
+ */
+
+package cryptography.rsa;
+
+import cryptography.DiffieHellman.DiffieHellman;
+
+import java.math.BigInteger;
+
+/*
+<https://habr.com/ru/post/119637/>
+<https://habr.com/ru/company/virgilsecurity/blog/459370/>
+RSA для встраиваемых систем (про длинную арифметику) <https://habr.com/ru/post/243425/>
+Уязвимости простой реализации <https://habr.com/ru/post/99376/>
+<https://habr.com/ru/post/61400/>
+*/
+
+public class RSA {
+    void genKey(DiffieHellman.PG pg){
+        BigInteger n = pg.p.multiply(pg.g);
+        BigInteger fi =
+                pg.p.subtract(BigInteger.ONE)
+                        .multiply(
+                                pg.g.subtract(BigInteger.ONE)
+                        );
+    }
+}
