4cce6b1b72e62611163efd5fd5f5d78c857c7442
Aleksandr Kolbasov <kolbasov.a.a@edu.mirea.ru>
2020-01-21 17:51:56 +0300

    Изменения в DH

--- src/cryptography/DiffieHellman/DHTest.java
+++ src/cryptography/DiffieHellman/DHTest.java
@@ -8,17 +8,19 @@ import java.math.BigInteger;
 
 public class DHTest {
     public static void main(String[] args) {
-        DiffieHellman dh1 = new DiffieHellman(), dh2 = new DiffieHellman();
+        DiffieHellman dhA, dhB;
+        BigInteger A, B, K1, K2;
 
-        BigInteger A,B;
-        DiffieHellman.PG pg = dh1.genPG();
-        A = dh1.initAndGet_A(pg);
-        B = dh2.initAndGet_A(pg);
+        DiffieHellman.PG pg = DiffieHellman.genPG();
+        dhA = new DiffieHellman(pg);
+        A = dhA.calculate_A();
+        dhB = new DiffieHellman(pg);
+        B = dhB.calculate_A();
 
-        BigInteger K1, K2;
-        K1 = dh1.calculateK(B);
-        K2 = dh2.calculateK(A);
+        K1 = dhA.calculate_K(B);
+        K2 = dhB.calculate_K(A);
 
-        System.out.printf("A:%s, B:%s\nK1:%s\nK2:%s\n\nK is correct - %s", A,B,K1,K2,K1.compareTo(K2) == 0);
+        System.out.printf("dhA: %s\ndhB: %s\nK1 == K2: %b", dhA, dhB, K1.compareTo(K2) == 0);
+        //System.out.printf("A:%s, B:%s\nK1:%s\nK2:%s\nK is correct - %s", A,B,K1,K2,K1.compareTo(K2) == 0);
     }
 }
