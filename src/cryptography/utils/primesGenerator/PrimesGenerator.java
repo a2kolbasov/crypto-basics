3fc84552337b844863673b8d1c6369987a81f723
Aleksandr Kolbasov <kolbasov.a.a@edu.mirea.ru>
2020-01-21 18:56:36 +0300

    Оптимизация PrimesGenerator

--- src/cryptography/utils/primesGenerator/PrimesGenerator.java
+++ src/cryptography/utils/primesGenerator/PrimesGenerator.java
@@ -11,17 +11,20 @@ public class PrimesGenerator {
     public static long getRandomPrime(){
         SecureRandom secureRandom = new SecureRandom();
         long randomLong = secureRandom.nextLong();
+        // randomLong % 2 != 0
+        randomLong |= 0b1;
         do {
             // TODO : SafePrime (p = 2 * q + 1)
             if (PrimalityTest.isProbablePrime(randomLong, 5))
                 return randomLong;
-            randomLong +=1;
+            // randomLong % 2 != 0 ==> +2
+            randomLong += 2;
         } while (true);
     }
 
 
     public static BigInteger getBigPrime(){
-        // TODO: регулировка длинны
+        // TODO: регулировка длинны (младший и старший биты)
         BigInteger prime =
                 new BigInteger(20, new SecureRandom())
                         .nextProbablePrime();
