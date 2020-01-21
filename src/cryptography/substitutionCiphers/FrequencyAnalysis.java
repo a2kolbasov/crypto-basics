d0cbb47f82952a68afffa623aafc5357777f3e70
Aleksandr Kolbasov <kolbasov.a.a@edu.mirea.ru>
2020-01-21 21:46:30 +0300

    Доделан частотный анализ

--- src/cryptography/substitutionCiphers/FrequencyAnalysis.java
+++ src/cryptography/substitutionCiphers/FrequencyAnalysis.java
@@ -1,14 +1,15 @@
-package cryptography.substitutionCiphers;
-
-import java.util.*;
-
 /*
  * Copyright © 2019 Alexander Kolbasov
  */
 
+package cryptography.substitutionCiphers;
+
+import java.util.*;
+
 // Можно добавить поиск наименьшей разности
 
 public class FrequencyAnalysis {
+    // Позволяет отбросить сторонние символы, знаки, буквы из других языков
     private final HashSet<Character> alphabet = new HashSet<>();
 
     public FrequencyAnalysis(String alphabet){
@@ -16,35 +17,87 @@ public class FrequencyAnalysis {
             this.alphabet.add(character);
     }
 
-    public LinkedList<String> toFrequencyMap(String text, int gramSize){
-        Map<String, Integer> frequency = new HashMap<>();
+    public Map<String, Integer> getGramFrequency(String text, int gramSize){
+        // <gram, frequency>
+        Map<String, Integer> frequencyMap = new HashMap<>();
 
         if (gramSize < 1)
-            return null;
+            return Collections.emptyMap();
+
         text = text.toLowerCase();
 
         for (int position = 0; position < text.length() - gramSize + 1; position++){
             String gram = text.substring(position, position + gramSize);
 
-            if (! containsInAlphabet(gram))
+            if (! isContainsInAlphabet(gram))
                 continue;
 
-            Integer sum = frequency.getOrDefault(gram, 0);
-            frequency.put(gram, sum + 1);
+            Integer frequency = frequencyMap.getOrDefault(gram, 0);
+            frequencyMap.put(gram, frequency + 1);
         }
-
-        LinkedList<Map.Entry<String, Integer>> ll = new LinkedList<>(frequency.entrySet());
-        Collections.sort(ll,(x,y) -> y.getValue().compareTo(x.getValue()));
-
-        System.out.println(ll);
-
-        LinkedList<String> l = new LinkedList<>();
-        for (Map.Entry<String, Integer> e : ll)
-            l.add(e.getKey());
-        return l;
+        return frequencyMap;
     }
 
-    private boolean containsInAlphabet(String gram){
+    public List<String> getSortedByFrequencyGramList(String text, int gramSize) {
+        if (gramSize < 1)
+            return Collections.emptyList();
+
+        Map<String, Integer> frequencyMap =  getGramFrequency(text, gramSize);
+
+        // Нужен для сортировки по Integer
+        LinkedList<Map.Entry<String, Integer>>
+                list = new LinkedList<>(frequencyMap.entrySet());
+        list.sort(
+                (x, y) -> y.getValue().compareTo( x.getValue() ));
+
+        // Итоговый List
+        LinkedList<String> result = new LinkedList<>();
+        for (Map.Entry<String, Integer> entry : list)
+            result.add(entry.getKey());
+        return result;
+    }
+
+    public String decryptText(String encryptedText, String referenceText, int gramSize) {
+//        if (gramSize < 1)
+//            return "";
+
+        // <Encrypted_gram, decrypted_gram>
+        Map<Character, Character> charMatch = new HashMap<>();
+
+        // Сопостовляем зашифрованные и расшифрованные символы
+        while (gramSize > 0 && charMatch.size() < alphabet.size()) {
+            Iterator<String> encryptedGrams = getSortedByFrequencyGramList(encryptedText, gramSize).iterator();
+            Iterator<String> referenceGrams = getSortedByFrequencyGramList(referenceText, gramSize).iterator();
+
+            while (encryptedGrams.hasNext() && referenceGrams.hasNext()) {
+                char[] encryptedCharsFromGram = encryptedGrams.next().toCharArray();
+                char[] referenceCharsFromGram = referenceGrams.next().toCharArray();
+                int point = 0;
+                for (char ch : encryptedCharsFromGram)
+                    // Если ещё не было такойже расшифрованной буквы
+                    if (! charMatch.containsValue(referenceCharsFromGram[point]))
+                        // И если не было такой же зашифрованной
+                        charMatch.putIfAbsent(ch, referenceCharsFromGram[point++]);
+            }
+            gramSize--;
+        }
+
+        // Расшифровываем текст
+        char[] replacement = encryptedText.toCharArray();
+        for (int position = 0; position < replacement.length; position++) {
+            boolean isUpper = Character.isUpperCase(replacement[position]);
+
+            Character replace = charMatch.getOrDefault(
+                    // Если нет расшифровки, то вернуть не меняя
+                    Character.toLowerCase(replacement[position]),
+                    replacement[position]
+            );
+            replacement[position] = isUpper ? Character.toUpperCase(replace) : replace;
+        }
+        return String.valueOf(replacement);
+    }
+
+    private boolean isContainsInAlphabet(String gram){
         for (Character character : gram.toCharArray()){
             if (! this.alphabet.contains(character))
                 return false;
--- src/cryptography/substitutionCiphers/ccTest.java
+++ src/cryptography/substitutionCiphers/ccTest.java
@@ -8,35 +8,32 @@ import cryptography.utils.Alphabets;
 import cryptography.utils.FileReader;
 
 import java.io.IOException;
-import java.util.Arrays;
-import java.util.Collections;
 import java.util.LinkedList;
-import java.util.Map;
+import java.util.List;
 
 public class ccTest {
     public static void main(String[] args) throws IOException {
+        /*
+         * Проверка работы шифра Цезаря
+         */
         String message = FileReader.readAll("глава.txt");
-
-        CaesarCipher cc = new CaesarCipher(Alphabets.RUSSIAN, 2);
+        CaesarCipher cc = new CaesarCipher(Alphabets.RUSSIAN, 3);
 
         String encryptedMessage = cc.encrypt(message);
         String decrypted = cc.decrypt(encryptedMessage);
-        System.out.println(encryptedMessage);
-        System.out.println("message == decryptedMessage " + message.equals(decrypted));
-        FrequencyAnalysis f = new FrequencyAnalysis(Alphabets.RUSSIAN);
-        LinkedList<String> mes, enMes;
-        mes= f.toFrequencyMap(FileReader.readAll("война_и_мир.txt"), 1);
-        enMes = f.toFrequencyMap(encryptedMessage, 1);
 
-//        System.out.println(mes);
-//        System.out.println(enMes);
-//        //System.out.println("1234");
-        for (Character ch : encryptedMessage.toCharArray()){
-            String sCh = String.valueOf(ch);
-            int index = enMes.indexOf(sCh);
-            System.out.print(
-                    index < 0 ? sCh : mes.get(index)
-            );
-        }
+        assert message.equals(decrypted);
+        System.out.println(encryptedMessage);
+        System.out.println(decrypted);
+
+        FrequencyAnalysis analysis = new FrequencyAnalysis(Alphabets.RUSSIAN);
+
+        /*
+         * Частотный анализ
+         */
+        String referenceText = FileReader.readAll("война_и_мир.txt");
+        System.out.println(
+                analysis.decryptText(encryptedMessage, referenceText, 1)
+        );
     }
 }
