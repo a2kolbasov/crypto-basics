54ee23974ee4f94e3801f0539a4fec5a5eee3c28
Aleksandr Kolbasov <kolbasov.a.a@edu.mirea.ru>
2020-01-21 14:46:47 +0300

    Тест простоты, генератор случайных простых чисел

---
+++ src/cryptography/utils/primesGenerator/PrimalityTest.java
@@ -0,0 +1,19 @@
+package cryptography.utils.primesGenerator;
+
+/*
+ * Copyright © 2019 Alexander Kolbasov
+ */
+
+import java.math.BigInteger;
+
+public class PrimalityTest {
+    public static boolean isProbablePrime(BigInteger number, int numberOfRounds){
+        // TODO : Реализовать алгоритм
+        return number.isProbablePrime(numberOfRounds);
+    }
+
+    public static boolean isProbablePrime(long number, int numberOfRounds){
+        return isProbablePrime(
+                BigInteger.valueOf(number), numberOfRounds);
+    }
+}
