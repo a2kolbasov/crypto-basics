ce48f2cf01dc102bc1ba458c7e88e73712500eaa
Aleksandr Kolbasov <kolbasov.a.a@edu.mirea.ru>
2020-01-24 22:48:14 +0300

    Изменён вывод теста DH

--- src/cryptography/tests/DHTest.java
+++ src/cryptography/tests/DHTest.java
@@ -14,14 +14,17 @@ public class DHTest {
         BigInteger A, B, K1, K2;
 
         DH.PG pg = DH.genPG();
+        System.out.printf("p: %s, g: %s\n", pg.p, pg.g);
+
         dhA = new DH(pg);
         A = dhA.genPublicKey();
+
         dhB = new DH(pg);
         B = dhB.genPublicKey();
 
         K1 = dhA.genSharedSecretKey(B);
         K2 = dhB.genSharedSecretKey(A);
 
-        System.out.printf("dhA: %s\ndhB: %s\nK1 == K2: %b", dhA, dhB, K1.compareTo(K2) == 0);
+        System.out.printf("A: %s\nB: %s\nK: %s, K1 == K2 - %b", A, B, K1, K1.compareTo(K2) == 0);
     }
 }
